15c4cbabea70e9675817c6af153b9a96c7d958c8
jacodegithub <dudeobama246@gmail.com>
2023-09-20 23:38:33 +0530

    added functionalities with javascript in picture in picture project

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+// PROMPT TO SELECT MEDIA STREAM, PASS TO VIDEO ELEMENT, THEN PLAY
+async function selectMediaStream() {
+
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play()
+        }
+    } catch(error) {
+        console.log('whoops!! something went wrong..')
+    }
+}
+
+button.addEventListener('click', async () => {
+    // DISABLE BUTTON
+    button.ariaDisabled = true;
+
+    // START PICTURE IN PICTURE
+    await videoElement.requestPictureInPicture();
+
+    // RESET BUTTON
+    button.disabled = false;
+})
+
+// ON LOAD
+selectMediaStream();
